c5ca9e293b543dc486a7e97bbfb39b4ac4eb58ff
Sinda <sindahaboubi104@gmail.com>
2023-11-15 21:48:43 +0100

    sinda_version

--- src/Personne.java
+++ src/Personne.java
@@ -1,14 +1,17 @@
 public class Personne {
 
     protected String nom;
+    protected String prenom;
+    protected int age;
 
-    public Personne(String nom) {
+    public Personne(String nom, String prenom, int age) {
         this.nom = nom;
+        this.prenom=prenom;
+        this.age=age;
     }
 
     @Override
     public String toString() {
-        return "je suis " + nom ;
+        return "Je suis " + nom + " "+prenom + " et j'ai "+ age +" ans";
     }
-
 }
