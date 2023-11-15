59d6b2e1bbbf20d487ba28c08336c8d4e5ef9cd3
hamed lahmar <lahmerhamed15@gmail.com>
2023-11-15 21:53:26 +0100

    ajout data scientist

---
+++ src/Data_sientist.java
@@ -0,0 +1,14 @@
+public class Data_sientist extends Personne {
+
+    public Data_sientist(String nom) {
+        super(nom);
+
+    }
+
+    @Override
+    public String toString() {
+
+        return super.toString()+" Data scientist";
+    }
+
+}
